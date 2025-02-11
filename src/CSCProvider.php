948030584ecc250c46857106513a98ee9f849bbb
Sanaullah Kiani <kianisanaullah@gmail.com>
2025-02-11 22:21:04 +0500

    updated

---
+++ src/CSCProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tuna976\csc;
+
+use Illuminate\Support\ServiceProvider;
+use Tuna976\CSC\Commands\ImportCountriesCommand;
+
+class CSCProvider extends ServiceProvider
+{
+    public function register()
+    {
+//        $this->mergeConfigFrom(__DIR__.'/../../config/country-importer.php', 'country-importer');
+    }
+
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../Database/migrations');
+        $this->commands([ImportCountriesCommand::class]);
+    }
+}
