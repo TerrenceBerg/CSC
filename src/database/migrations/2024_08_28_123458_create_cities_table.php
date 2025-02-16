5100a91f3f36900b9d18babc01acb1fab83ed013
TerrenceBerg <Terrence@976-TUNA.com>
2025-02-16 12:24:40 -0800

    fix migration to add more fields

--- src/database/migrations/2024_08_28_123458_create_cities_table.php
+++ src/database/migrations/2024_08_28_123458_create_cities_table.php
@@ -3,15 +3,22 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+ 
 return new class extends Migration {
     public function up(): void
     {
         Schema::create('cities', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('zip_code')->nullable();
+            $table->string('city_name');
+            $table->string('zip_code');            
             $table->foreignId('state_id')->constrained('states')->onDelete('cascade');
+            $table->string('state_name');
+            $table->string('state_postal_abbreviation');
+            $table->string('county_name');
+            $table->string('county_fips');
+            $table->string('timezone');
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
             $table->timestamps();
         });
     }
